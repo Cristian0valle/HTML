5bc247d23e573830d0bf5dfee76cef72cdd259f3
Cristian Ovalle <115573088+MalditoValle@users.noreply.github.com>
2023-05-11 03:05:50 -0400

    Add files via upload
    E-commerce sencillo, versión 1.0

---
+++ E-commerce/Login/script.js
@@ -0,0 +1,4 @@
+function redirigir() {
+    window.location.href = "C:\Users\56978\Desktop\Productos\Ingreso\index.html"; // Ruta relativa
+    // window.location.href = "/ruta/otra-pagina.html"; // Ruta absoluta
+  }
---
+++ E-commerce/script.js
@@ -0,0 +1,4 @@
+function redirigir() {
+    window.location.href = "Ingreso/index.html"; // Ruta relativa
+    // window.location.href = "/ruta/otra-pagina.html"; // Ruta absoluta
+  }
